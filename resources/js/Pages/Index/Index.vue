bfd3b4d22064ce0f4386dff9f472b3b406e19fb8
Daphne Tee <zhengpeiwendaphnne@gmail.com>
2023-11-01 17:48:49 +0800

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Index</div>
+  <Link href="/hello">Show Page</Link>
+  <div>
+    The message is {{ message }}
+  </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+import MainLayouts from '../../Layouts/MainLayouts.vue'
+defineProps({ message: String })
+</script>
---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-for="listing in listings" :key="listing.id">
+        <div>
+        <!-- <Link :href="`/listing/${listing.id}`"> -->
+          <Link :href="route('listing.show', listing.id)">
+        <ListingAddress :listing="listing" />
+      </Link>
+        </div>
+      <div>
+        <!-- <Link :href="`/listing/${listing.id}/edit`">Edit</Link> -->
+        <Link :href="route('listing.edit',{listing:listing.id})">Edit</Link>
+      </div>
+      <div>
+        <!-- <Link :href="`/listing/${listing.id}`" method="DELETE" as="button">Delete</Link> -->
+        <Link :href="route('listing.destroy', {listing:listing.id} )" method="DELETE" as="button">Delete</Link>
+      </div>
+    </div>
+  </template>
+  
+
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3'
+import ListingAddress from '../../Components/ListingAddress.vue';
+defineProps({
+listings: Array,
+})
+</script>
